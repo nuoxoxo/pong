1a0b608eadc9cfd69880e4e225a2786d1f70b92c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-06 18:57:57 +0200

    fix sketch

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -3,49 +3,50 @@ let rightscore = 0
 let paddle_speed = 21
 
 function setup() {
-  createCanvas(1200, 800)
-  ball = new Ball()
-  left = new Paddle(true)
-  right = new Paddle(false)
+    createCanvas(1200, 800)
+    ball = new Ball()
+    left = new Paddle(true)
+    right = new Paddle(false)
 }
 
 function draw() {
-  background(0)
+    background(0)
 
-  ball.checkPaddleRight(right)
-  ball.checkPaddleLeft(left)
+    ball.checkPaddleRight(right)
+    ball.checkPaddleLeft(left)
 
-  left.show()
-  right.show()
-  left.update()
-  right.update()
+    left.show()
+    right.show()
+    left.update()
+    right.update()
 
-  ball.update()
-  ball.edges()
-  ball.show()
+    ball.update()
+    ball.edges()
+    ball.show()
 
-  fill(255)
-  textSize(32)
-  text(leftscore, 32, 40)
-  text(rightscore, width - 64, 40)
+    fill(255)
+    textSize(32)
+    text(leftscore, 32, 40)
+    text(rightscore, width - 64, 40)
 }
 
 function keyReleased() {
-  left.move(0)
-  right.move(0)
+    left.move(0)
+    right.move(0)
 }
 
 function keyPressed() {
-  console.log(key)
-  if (key == "w") {
-    left.move(-paddle_speed)
-  } else if (key == "s") {
-    left.move(paddle_speed)
-  }
+    console.log(key)
+    if (key == "w") {
+        left.move(-paddle_speed)
+    } else if (key == "s") {
+        left.move(paddle_speed)
+    }
 
-  if (key == "ArrowUp") {
-    right.move(-paddle_speed)
-  } else if (key == "ArrowDown") {
-    right.move(paddle_speed)
-  }
+    if (key == "ArrowUp") {
+        right.move(-paddle_speed)
+    } else if (key == "ArrowDown") {
+        right.move(paddle_speed)
+    }
 }
+
